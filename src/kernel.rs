9205820ee92487523efac4ff3e5d579b61f9c912
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-24 16:51:30 +0900

    test: separate signature self test from boot

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,7 +1,6 @@
 use crate::capability::path::{
     register_service_paths, PathRights, PATH_CREATE, PATH_LIST, PATH_READ, PATH_WRITE,
 };
-use crate::capability::{Capability, CapabilitySet};
 use crate::result::handle_kernel_error;
 use crate::result::{Kernel, Process};
 use crate::util::log::LogLevel;
@@ -134,37 +133,6 @@ fn kernel_main() -> ! {
             ("/testdata", PathRights::new(PATH_READ | PATH_LIST)),
         ];
         let _ = register_service_paths(manager_pid, &service_paths);
-
-        let mut signature_allow_caps = CapabilitySet::empty();
-        signature_allow_caps.insert(Capability::ProcessSpawn);
-        let signature_allow_pid = crate::syscall::exec::exec_kernel_with_name_and_caps(
-            "/captest.bin",
-            "signature-allow-test",
-            signature_allow_caps,
-            crate::task::PrivilegeLevel::User,
-        );
-        if signature_allow_pid == 0 || signature_allow_pid & (1u64 << 63) != 0 {
-            crate::error!(
-                "signature allow test failed: ret={:#x}",
-                signature_allow_pid
-            );
-        } else {
-            crate::info!(
-                "signature allow test launched pid={:#x}",
-                signature_allow_pid
-            );
-        }
-
-        let signature_deny_ret =
-            crate::syscall::exec::exec_kernel_with_name("/unsigned.bin", "signature-deny-test");
-        if signature_deny_ret == 0 || signature_deny_ret & (1u64 << 63) == 0 {
-            crate::error!(
-                "signature deny test unexpectedly succeeded: ret={:#x}",
-                signature_deny_ret
-            );
-        } else {
-            crate::info!("signature deny test rejected ret={:#x}", signature_deny_ret);
-        }
     } else {
         crate::warn!(
             "Failed to register service manager (ret={:#x})",
